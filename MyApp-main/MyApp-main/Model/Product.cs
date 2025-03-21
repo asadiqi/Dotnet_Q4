3d45eaf0fabcff923a865200f1c9cd82052a6e99
asadiqi <asadiqi@helb-prigogine.be>
2025-03-21 22:59:40 +0100

    Creation model Product

---
+++ MyApp-main/MyApp-main/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Model
+{
+    public class Product
+    {
+
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Picture { get; set; } = string.Empty;
+    }
+}
+
+
+
--- MyApp-main/MyApp-main/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyApp.AssemblyInfo.cs
+++ MyApp-main/MyApp-main/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+97636704d768726554c9cb2f3a87caee2d96e890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9ebb63ff850f7577f156dd88032802aac67e1652")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
