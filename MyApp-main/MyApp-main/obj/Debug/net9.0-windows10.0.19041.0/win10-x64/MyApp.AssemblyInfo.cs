2527bc27ffe2502c1886108926851940326ce076
asadiqi <asadiqi@helb-prigogine.be>
2025-04-05 17:08:16 +0200

    Page All Product Stylisé

--- MyApp-main/MyApp-main/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyApp.AssemblyInfo.cs
+++ MyApp-main/MyApp-main/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+577ecaf686f951ae919986a6b360173e045d530c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1c41b5d2b808685f1b6e1c14fa35d798e08c634d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
