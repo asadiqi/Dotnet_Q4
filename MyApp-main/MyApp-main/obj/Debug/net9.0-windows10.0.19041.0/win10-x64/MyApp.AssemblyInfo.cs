061249e0597a28655a586e87d14e58363f86ffee
asadiqi <asadiqi@helb-prigogine.be>
2025-05-03 20:01:42 +0200

    Pas de frame dans les views

--- MyApp-main/MyApp-main/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyApp.AssemblyInfo.cs
+++ MyApp-main/MyApp-main/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MyApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+674b750c74290db6e097f57133a2bfe311cd531d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e1da98c3588358acf3ddfcbfa4049c8eaa575f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
